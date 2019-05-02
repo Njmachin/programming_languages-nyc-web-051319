9064bb6813266f0ac788ba83b36b8352fc74322d
Nicholas Machin <nicholasjohnmachin@gmail.com>
2019-05-02 20:28:25 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -47,39 +47,3 @@ languages = {
  
   }
 }
-This is the hash you will be operating on and reformatting, programmatically––meaning you will not simply re-write the hash in order to get the tests passing.
-
-Iterate over the hash and build a new hash that has the languages as keys that point to a value of a hash that describes the type and style.
-
-It should match the following format:
-
-{
-  :ruby => {
-    :type => "interpreted",
-    :style => [:oo]
-  },
-  :javascript => {
-    :type => "interpreted",
-    :style => [:oo, :functional]
-  },
-  :python => {
-    :type => "interpreted",
-    :style => [:oo]
-  },
-  :java => {
-    :type => "compiled",
-    :style => [:oo]
-  },
-  :clojure => {
-    :type => "compiled",
-    :style => [:functional]
-  },
-  :erlang => {
-    :type => "compiled",
-    :style => [:functional]
-  },
-  :scala => {
-    :type => "compiled",
-    :style => [:functional]
-  }
-}
